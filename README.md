74809fca5479646544f29ab8881b80611000507d
Omar Khaled <omarkhaled6154@gmail.com>
2022-10-29 01:32:46 +0200

    Version 2.0

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## Changelog
 
+### Version 2.0
+
+- Added Hourly Forecast data with chart.js
+
 ### Version 1.0.1
 
 - Fixed Responsiveness
--- public/js/script.js
+++ public/js/script.js
@@ -8,10 +8,12 @@ const bg = document.getElementsByTagName("BODY")[0];
 const weathercontainer = document.querySelector(".weather-container");
 const title = document.querySelector(".app-title");
 const key = "82005d27a116c2880c8f0fcb866998a0";
+const hourlyw = document.querySelector(".hourly");
 let night;
 const Kelvin = 273
 const weather = {};
-
+const hourlyweather = {};
+let nightcheck;
 
 // Date Display
 dateselector.innerHTML = new Date().toDateString();
@@ -20,6 +22,10 @@ dateselector.innerHTML = new Date().toDateString();
 weather.temperature = {
     unit: "celsius",
 }
+
+hourlyweather.temperature = {
+    unit: "celsius",
+};
 
 // If the user clicks on the temperature value, it will be converted to fahrenheit (if celsius)
 // or celsius (if fahrenheit), otherwise it won't work
@@ -48,7 +54,8 @@ if ("geolocation" in navigator) {
     navigator.geolocation.getCurrentPosition(function (position) {
         let latitude = position.coords.latitude;
         let longitude = position.coords.longitude;
-        getWeather(latitude, longitude);
+        getUserWeather(latitude, longitude);
+        getHourlyWeather(latitude, longitude);
     })
 } else {
     // If no, an error will appear to the user
@@ -56,8 +63,8 @@ if ("geolocation" in navigator) {
 }
 
 
-// Function to fetch weather information from the API
-async function getWeather(latitude, longitude) {
+// Function to fetch user's location weather information from the API
+async function getUserWeather(latitude, longitude) {
 
     // OpenWeatherApp API is used to retrieve weather information based on user's location (latitude and longitude)
 
@@ -80,35 +87,33 @@ async function getWeather(latitude, longitude) {
             // The weather's condition's in a certain country
             weather.country = data.sys.country;
         }).then(() => {
+            nightcheck = weather.icon.toString().charAt(2)
+            if (nightcheck === "n") {
+                night = true;
+            }
+            else {
+                night = false;
+            }
             Display();
         });
 
 
     // Display the weather information
     function Display() {
-        const nightcheck = weather.icon.toString().charAt(2)
         // Check if current time is day or night (ex: 6 PM - 6 AM is night, otherwise, it's day)
-        if (nightcheck === "n") {
-            night = true;
+        if (night) {
             // Changing the text color to white during the night for better accessibility
             weathercontainer.style.color = "#FFFFFF";
             title.style.color = "#FFFFFF";
             title.innerHTML = "Tonight's Weather";
             dateselector.style.color = "#FFFFFF";
+            icon.innerHTML = `<img src="img/weathericons/night/${weather.icon}.png" alt="${weather.icon}"/>`;
         } else {
-            night = false;
             // Changing the text color to black during the day for better accessibility
             weathercontainer.style.color = "#000000";
             title.style.color = "#000000";
             dateselector.style.color = "#000000";
             title.innerHTML = "Today's Weather";
-        }
-        // if the current time is night, the white icons will be displayed
-        if (night) {
-            icon.innerHTML = `<img src="img/weathericons/night/${weather.icon}.png" alt="${weather.icon}"/>`;
-        }
-        // if the current time is day, the black icons will be displayed
-        else {
             icon.innerHTML = `<img src="img/weathericons/day/${weather.icon}.png" alt="${weather.icon}"/>`;
         }
         bg.style.backgroundImage = `url("./img/${weather.icon}.jpg")`;
@@ -118,4 +123,120 @@ async function getWeather(latitude, longitude) {
         description.innerHTML = descr;
         loc.innerHTML = `${weather.city}, ${weather.country}`;
     }
+}
+
+async function getHourlyWeather(latitude, longitude) {
+
+    let api = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}`
+
+    fetch(api)
+        .then((res) => {
+            let data = res.json();
+            console.log(data);
+            return data;
+        })
+        .then((data) => {
+            let arrtemp = []
+            let arrdt = []
+            let arricon = []
+            for (let i = 0; i < 8; i++) {
+                arrtemp[i] = JSON.parse(Math.floor(data.list[i].main.temp - Kelvin))
+                let unix = JSON.parse(data.list[i].dt)
+                // arricon[i] = JSON.parse(data.list[i].
+                let date = new Date(unix * 1000)
+                let hours = date.getHours()
+                let minutes = date.getMinutes()
+                arrdt[i] = hours + ":" + minutes + "0"
+            }
+            let myChart = document.getElementById('myChart').getContext('2d');
+            if (night) {
+                let threehoursChart = new Chart(myChart, {
+                    type: 'line',
+                    data: {
+                        labels: arrdt,
+                        datasets: [{
+                            label: 'Hours',
+                            data: arrtemp,
+                            backgroundColor: 'white',
+                            fill: false,
+                            borderColor: 'white',
+                            color: "#ffffff"
+                        },
+                        ]
+                    },
+                    options: {
+                        legend: {
+                            labels: {
+                                fontColor: "blue",
+                                fontSize: 18
+                            }
+                        },
+                        scales: {
+                            y: {  // not 'yAxes: [{' anymore (not an array anymore)
+                              ticks: {
+                                color: "white", // not 'fontColor:' anymore
+                                // fontSize: 18,
+                                stepSize: 1,
+                                beginAtZero: true
+                              }
+                            },
+                            x: {  // not 'xAxes: [{' anymore (not an array anymore)
+                              ticks: {
+                                color: "white",  // not 'fontColor:' anymore
+                                //fontSize: 14,
+                                stepSize: 1,
+                                beginAtZero: true
+                              }
+                            }
+                          },
+                        plugins: {
+                        title: {
+                            display: true,
+                            text: '3-Hours Forecast',
+                            fontSize: 25,
+                            color: "#ffffff"
+                        },
+                        responsive: true,
+                        legend: {
+                            labels: {
+                                color: "white",
+                            }
+                        },
+                    }
+                    },
+                });
+            }
+            else {
+                let threehoursChart = new Chart(myChart, {
+                    type: 'line',
+                    data: {
+                        labels: arrdt,
+                        datasets: [{
+                            label: 'Hours',
+                            data: arrtemp,
+                            backgroundColor: 'black',
+                            fill: false,
+                            borderColor: 'rgb(0,0,0)',
+                            color: "#ffffff"
+                        },
+                        ],
+                    },
+                    options: {
+                        plugins: {
+                            title: {
+                                display: true,
+                                text: '3-Hours Forecast',
+                                color: "white"
+                            },
+                            responsive: true,
+                            legend: {
+                                labels: {
+                                    color: "white",
+                                }
+                            },
+                        },
+                    }
+                })
+            }
+        })
 }
